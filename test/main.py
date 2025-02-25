ad3b4eb27a78327cc87febe8632f1a3dd5cbf01b
Dominick Rodriguez <DominickRodriguez63@gmail.com>
2025-02-25 05:35:24 -0500

    2/25

--- test/main.py
+++ test/main.py
@@ -1,8 +1,75 @@
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import font
+from tkinter import font,colorchooser
 
-#go test
+#Functionality part
+
+def new_file():
+    textarea.delete(0.0,END)
+
+
+fontSize=12
+fontStyle='Consolas'
+def font_style(event):
+    global fontStyle
+    fontStyle=font_family_variable.get()
+    textarea.config(font=(fontStyle,fontSize))
+
+def font_size(event):
+    global fontSize
+    fontSize = size_variable.get()
+    textarea.config(font=(fontStyle, fontSize))
+
+def bold_text():
+    text_property=font.Font(font=textarea['font']).actual()
+    if text_property['weight']=='normal':
+        textarea.config(font=(fontStyle,fontSize,'bold'))
+
+    if text_property['weight']=='bold':
+        textarea.config(font=(fontStyle,fontSize,'normal'))
+
+def italic_text():
+    text_property = font.Font(font=textarea['font']).actual()
+    #print(text_property)
+    if text_property['slant'] == 'roman':
+        textarea.config(font=(fontStyle, fontSize, 'italic'))
+
+    if text_property['slant'] == 'italic':
+        textarea.config(font=(fontStyle, fontSize, 'roman'))
+
+def underline_text():
+    text_property = font.Font(font=textarea['font']).actual()
+    if text_property['underline'] == 0:
+        textarea.config(font=(fontStyle, fontSize, 'underline'))
+
+    if text_property['underline'] == 1:
+        textarea.config(font=(fontStyle, fontSize))
+
+def color_select():
+    color=colorchooser.askcolor()
+    textarea.config(fg=color[1])
+
+def align_right():
+    data = textarea.get(0.0, END)
+    textarea.tag_config('right', justify=RIGHT)
+    textarea.delete(0.0, END)
+    textarea.insert(INSERT, data, 'right')
+
+
+def align_left():
+    data = textarea.get(0.0, END)
+    textarea.tag_config('left', justify=LEFT)
+    textarea.delete(0.0, END)
+    textarea.insert(INSERT, data, 'left')
+
+def align_center():
+    data = textarea.get(0.0, END)
+    textarea.tag_config('center', justify=CENTER)
+    textarea.delete(0.0, END)
+    textarea.insert(INSERT, data, 'center')
+
+
 root=Tk()
 root.title('PwndaTxt')
 root.geometry('1200x620+10+10')
@@ -18,7 +85,7 @@ filemenu=Menu(menubar, tearoff=False)
 menubar.add_cascade(label = 'File', menu=filemenu)
 
 newImage=PhotoImage(file='new.png')
-filemenu.add_command(label='New',accelerator='Ctrl+N',image=newImage,compound=LEFT)
+filemenu.add_command(label='New',accelerator='Ctrl+N',image=newImage,compound=LEFT,command=new_file)
 
 openImage=PhotoImage(file='open.png')
 filemenu.add_command(label='Open',accelerator='Ctrl+O',image=openImage,compound=LEFT)
@@ -89,50 +156,55 @@ tool_bar = Label(root)
 tool_bar.pack(side=TOP, fill=X)
 font_families=font.families()
 font_family_variable=StringVar()
-
+#Fonts
 fontfamily_Combobox=Combobox(tool_bar, width=30, values=font_families, state='readonly',textvariable=font_family_variable)
-fontfamily_Combobox.current(font_families.index('Arial')) #Default font
+fontfamily_Combobox.current(font_families.index('Consolas')) #Default font
 fontfamily_Combobox.grid(row=0,column=0,padx=5)
+fontfamily_Combobox.bind('<<ComboboxSelected>>', font_style)
 
+#Font sizes
 size_variable=IntVar()
 font_size_Combobox=Combobox(tool_bar,width=14,textvariable=size_variable,state='readonly',values=tuple(range(8,81)))
 font_size_Combobox.current(4)
 font_size_Combobox.grid(row=0,column=1,padx=5)
+font_size_Combobox.bind('<<ComboboxSelected>>', font_size)
+
 
 #buttons section
 
 boldImage=PhotoImage(file= 'bold.png')
-boldButton=Button(tool_bar, image=boldImage)
+boldButton=Button(tool_bar, image=boldImage,command=bold_text)
 boldButton.grid(row=0,column=2,padx=5)
 
+
 italicImage=PhotoImage(file= 'italic.png')
-italicButton=Button(tool_bar, image=italicImage)
+italicButton=Button(tool_bar, image=italicImage,command=italic_text)
 italicButton.grid(row=0,column=3,padx=5)
 
 underlineImage=PhotoImage(file= 'underline.png')
-underlineButton=Button(tool_bar, image=underlineImage)
+underlineButton=Button(tool_bar, image=underlineImage,command=underline_text)
 underlineButton.grid(row=0,column=4,padx=5)
 
 fontColorImage=PhotoImage(file= 'font_color.png')
-fontColorButton=Button(tool_bar, image=fontColorImage)
+fontColorButton=Button(tool_bar, image=fontColorImage, command=color_select)
 fontColorButton.grid(row=0,column=5,padx=5)
 
 leftAlignImage=PhotoImage(file= 'left.png')
-leftAlignButton=Button(tool_bar, image=leftAlignImage)
+leftAlignButton=Button(tool_bar, image=leftAlignImage, command=align_left)
 leftAlignButton.grid(row=0,column=6,padx=5)
 
 centerAlignImage=PhotoImage(file= 'center.png')
-centerAlignButton=Button(tool_bar, image=centerAlignImage)
+centerAlignButton=Button(tool_bar, image=centerAlignImage, command=align_center)
 centerAlignButton.grid(row=0,column=7,padx=5)
 
 rightAlignImage=PhotoImage(file= 'right.png')
-rightAlignButton=Button(tool_bar, image=rightAlignImage)
+rightAlignButton=Button(tool_bar, image=rightAlignImage, command=align_right)
 rightAlignButton.grid(row=0,column=8,padx=5)
 
 scrollbar=Scrollbar(root)
 scrollbar.pack(side=RIGHT, fill=Y)
 
-textarea=Text(root, yscrollcommand=scrollbar.set, font=('arial',12))
+textarea=Text(root, yscrollcommand=scrollbar.set, font=('Consolas',12))
 textarea.pack(fill=BOTH,expand=True)
 scrollbar.config(command=textarea.yview)
 
